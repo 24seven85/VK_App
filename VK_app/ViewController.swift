8980e8c4067847a97f62eb6641922f01dc12881a
maksimkjaulakis <24seven@live.ru>
2021-01-30 08:50:14 +0200

    Initial Commit

---
+++ VK_app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VK_app
+//
+//  Created by Maksim Kjaulakis on 30.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
