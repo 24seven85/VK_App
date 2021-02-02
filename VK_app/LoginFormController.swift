ba9cc4931f0fabe73200de2bc433403ff566edf3
maksimkjaulakis <24seven@live.ru>
2021-02-02 13:37:00 +0200

    lesson2

--- VK_app/LoginFormController.swift
+++ VK_app/LoginFormController.swift
@@ -65,20 +65,56 @@ class LoginFormController: UIViewController {
     }
     
     
-    @IBAction func loginButtonPressed(_ sender: Any) {
-        // Получаем текст логина
-                let login = loginInput.text!
-                // Получаем текст-пароль
-                let password = passwordInput.text!
-                
-                // Проверяем, верны ли они
-                if login == "user1" && password == "1234" {
-                    print("успешная авторизация, вы вошли как \(login)")
-                } else {
-                    print("ошибка авторизации")
-                }
-
-    }
+//    @IBAction func loginButtonPressed(_ sender: Any) {
+//        // Получаем текст логина
+//                let login = loginInput.text!
+//                // Получаем текст-пароль
+//                let password = passwordInput.text!
+//
+//                // Проверяем, верны ли они
+//                if login == "user1" && password == "1234" {
+//                    print("успешная авторизация, вы вошли как \(login)")
+//                } else {
+//                    print("ошибка авторизации")
+//                }
+//
+//    }
     
+    
+    override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
+            // Проверяем данные
+            let checkResult = checkUserData()
+            
+            // Если данные не верны, покажем ошибку
+            if !checkResult {
+                showLoginError()
+            }
+            
+            // Вернем результат
+            return checkResult
+        }
+        
+        func checkUserData() -> Bool {
+            guard let login = loginInput.text,
+                let password = passwordInput.text else { return false }
+            
+            if login == "user1" && password == "1234" {
+                return true
+            } else {
+                return false
+            }
+        }
+        
+        func showLoginError() {
+            // Создаем контроллер
+            let alter = UIAlertController(title: "Ошибка", message: "Введены не верные данные пользователя", preferredStyle: .alert)
+            // Создаем кнопку для UIAlertController
+            let action = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            // Добавляем кнопку на UIAlertController
+            alter.addAction(action)
+            // Показываем UIAlertController
+            present(alter, animated: true, completion: nil)
+        }
+
 
 }
